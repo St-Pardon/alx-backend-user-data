684f5dcc23f5b49bbeef0975b8358cf58d17579e
St Pardon <okc4pardon@gmail.com>
2023-03-02 16:58:35 +0100

    :white_check_mark: completed task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+'''personal data protection module
 '''
-'''
+import re
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''obfuscate log messages
+    '''
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
