6a4f3702f9039ce97705c948cfc073044fecaa53
St Pardon <okc4pardon@gmail.com>
2023-03-07 18:32:35 +0100

    :updated auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 '''
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -12,7 +13,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Ensures authentication is required
         '''
-        return
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''includes authorization header to http req
