45ab1a24140259e5c3fe32778f5b89ae6b2cc9f9
St Pardon <okc4pardon@gmail.com>
2023-03-07 19:30:47 +0100

    :white_check_mark: completed all mandatory tasks

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,10 @@
 '''
 from .auth import Auth
 import re
-from typing import Tuple
+from typing import Tuple, TypeVar
 import base64
 import binascii
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -59,3 +60,29 @@ class BasicAuth(Auth):
                 password = field_match.group('password')
                 return user, password
         return None, None
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        '''Retrieves a user based on the user's authentication credentials.
+        '''
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''Retrieves the user from a request.
+        '''
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
