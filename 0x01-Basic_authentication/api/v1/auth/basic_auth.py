dfab248e6c27c64597c6cb0bf9cf3edc4e96d5cd
St Pardon <okc4pardon@gmail.com>
2023-03-07 19:16:46 +0100

    :white_check_mark: added method for base64 decode and user extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 '''
 from .auth import Auth
 import re
+from typing import Tuple
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,3 +24,38 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+    ) -> str:
+        '''Decodes a base64-encoded authorization header.
+        '''
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+    ) -> Tuple[str, str]:
+        '''Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow.
+        '''
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
