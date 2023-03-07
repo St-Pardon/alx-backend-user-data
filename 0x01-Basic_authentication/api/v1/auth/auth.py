9433439a9de9bb3e01d6032fb43226f7b47dd1c7
St Pardon <okc4pardon@gmail.com>
2023-03-07 18:47:03 +0100

    :white_check_mark: updated auth class and included auth handler in app.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,6 +29,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         '''includes authorization header to http req
         '''
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
