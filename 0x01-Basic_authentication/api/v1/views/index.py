dcc3e0e69e24fd9e9afd4a9e3e8180571669a71a
St Pardon <okc4pardon@gmail.com>
2023-03-07 17:17:43 +0100

    :hammer: included the endpoint to test the error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the number of each objects
+    """
+    abort(401)
