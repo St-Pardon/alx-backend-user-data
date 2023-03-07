44cdd860b96fc230cecfcfbfadf4ccca3c68812c
St Pardon <okc4pardon@gmail.com>
2023-03-07 19:02:12 +0100

    :hammer: included base64 extraction method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,22 @@
 '''Basic authentication
 '''
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     '''Basic Auth class
     '''
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        '''
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
