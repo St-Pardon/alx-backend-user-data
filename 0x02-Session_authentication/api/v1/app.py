20f5bb2517187f9a6a74c04b00a3f1e0a218818d
St Pardon <okc4pardon@gmail.com>
2023-03-10 18:11:18 +0100

    :wrench: included missing path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,6 +55,7 @@ def auth_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
