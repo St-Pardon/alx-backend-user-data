c24fd7ee28cc4affa8e8617d5b313fab0e23cd3c
St Pardon <okc4pardon@gmail.com>
2023-03-10 17:51:07 +0100

    :hammer: implemented requirement for task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,16 @@
 '''Session Authentication module
 '''
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth (Auth):
     '''Session auth class
     '''
-    pass
+    def create_session(self, user_id: str = None) -> str:
+        '''Method for creating sessions
+        '''
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
